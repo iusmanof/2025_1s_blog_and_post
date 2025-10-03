28cf3f8cd8957dd6a142df17fe34913974c4a229
iusmanof <iusmanof@gmail.com>
2025-10-03 23:22:31 +0300

    v1.9 versel simple app mongo

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,68 @@
-import express, {Request, Response} from "express";
-
-import {BlogRouter} from "./routers/BlogRouters";
-import {PostRouter} from "./routers/PostRouters";
-import {runDB} from "./repositories/db";
-import {blogDataAccessLayerMongoDB} from "./dataAccessLayer/blog-data-access-layer-mongodb";
-import {postAccessLayerMongoDB} from "./dataAccessLayer/post-data-access-layer-mongodb";
+// import express, {Request, Response} from "express";
+//
+// import {BlogRouter} from "./routers/BlogRouters";
+// import {PostRouter} from "./routers/PostRouters";
+// import {getBlogCollection, runDB} from "./repositories/db";
+// import {blogDataAccessLayerMongoDB} from "./dataAccessLayer/blog-data-access-layer-mongodb";
+// import {postAccessLayerMongoDB} from "./dataAccessLayer/post-data-access-layer-mongodb";
+//
+// const app = express()
+// const port = process.env.port || 3000
+//
+// app.use(express.json());
+// app.use('/blogs', BlogRouter)
+// app.use('/posts', PostRouter)
+//
+// app.get('/', async (req: Request, res: Response) => {
+//     await res.send('blogs api')
+//   })
+//
+// app.delete('/testing/all-data', async (req: Request, res: Response) => {
+//   blogDataAccessLayerMongoDB.deleteAllBlogs()
+//   postAccessLayerMongoDB.deleteAllPosts()
+//   res.status(204).send("All data is deleted")
+// })
+//
+// const startApp = async () => {
+//     await runDB();
+//
+//     app.listen(port, () => {
+//         console.log(`App listening on port ${port}`)
+//     })
+// }
+//
+// startApp()
+//
+// export default app;
+import express, { Request, Response } from 'express'
+import { runDB, getBlogCollection } from './repositories/db'
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
-app.use(express.json());
-app.use('/blogs', BlogRouter)
-app.use('/posts', PostRouter)
+app.use(express.json())
 
-app.get('/', async (req: Request, res: Response) => {
-    await res.send('blogs api')
-  })
-
-app.delete('/testing/all-data', async (req: Request, res: Response) => {
-  blogDataAccessLayerMongoDB.deleteAllBlogs()
-  postAccessLayerMongoDB.deleteAllPosts()
-  res.status(204).send("All data is deleted")
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from API')
 })
 
+app.get('/blogs', async (req: Request, res: Response) => {
+    const blogs = await getBlogCollection().find({}).toArray()
+    res.json(blogs)
+})
+
+app.delete('/testing/all-data', async (req: Request, res: Response) => {
+    await getBlogCollection().deleteMany({})
+    res.status(204).send()
+})
+
+// Старт приложения
 const startApp = async () => {
-    await runDB();
+    await runDB()
 
     app.listen(port, () => {
-        console.log(`App listening on port ${port}`)
+        console.log(`🚀 Server is running on http://localhost:${port}`)
     })
 }
 
 startApp()
-
-export default app;
--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,25 +1,35 @@
-import { MongoClient } from 'mongodb'
+import {Collection, MongoClient} from 'mongodb'
+import {PostMongoDb} from "../model_types/PostModel";
 import { BlogMongoDb } from "../model_types/BlogModel";
 import dotenv from 'dotenv'
-import {PostMongoDb} from "../model_types/PostModel";
+
 dotenv.config()
 
-const url: string | undefined = process.env.MONGODB_URI
-const dbName: string | undefined= process.env.DB_NAME
-console.log(url)
-console.log(dbName)
-console.log("process.env.MONGODB_URI" + process.env.MONGODB_URI)
-console.log("process.env.DB_NAME" + process.env.DB_NAME)
+const url = process.env.MONGODB_URI
+const dbName = process.env.DB_NAME
 
-if (!url) throw new Error("MONGODB_URI\n is not defined");
+if (!url) throw new Error('MONGODB_URI is not defined')
+if (!dbName) throw new Error('DB_NAME URL is not defined')
 
 const client = new MongoClient(url)
-export const blogCollection = client.db(dbName).collection<BlogMongoDb>('blogs')
-export const postCollection = client.db(dbName).collection<PostMongoDb>('posts')
 
-export const  runDB = async () =>{
+let blogCollection: Collection<BlogMongoDb>
+let postCollection: Collection<PostMongoDb>
+
+// console.log(url)
+// console.log(dbName)
+// console.log("process.env.MONGODB_URI" + process.env.MONGODB_URI)
+// console.log("process.env.DB_NAME" + process.env.DB_NAME)
+
+// export const blogCollection = client.db(dbName).collection<BlogMongoDb>('blogs')
+// export const postCollection = client.db(dbName).collection<PostMongoDb>('posts')
+
+export const runDB = async () =>{
     try {
         await client.connect();
+        const db = client.db(dbName);
+        blogCollection = db.collection<BlogMongoDb>('blogs')
+        postCollection = db.collection<PostMongoDb>('posts')
         console.log("Connect successfully to server")
     } catch (e) {
         console.error("Don't connect to server")
@@ -27,3 +37,6 @@ export const  runDB = async () =>{
         await client.close();
     }
 }
+
+export const getBlogCollection = () =>blogCollection
+export const getPostCollection = () =>postCollection
