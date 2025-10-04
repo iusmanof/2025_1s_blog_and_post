c4c5770cfbd8ded743aacee3728a86e8da500c2b
iusmanof <iusmanof@gmail.com>
2025-10-04 23:35:05 +0300

    v3.8 eslint comment

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import eslintPluginTs from "@typescript-eslint/eslint-plugin";
-import parserTs from "@typescript-eslint/parser";
-import prettierPlugin from "eslint-plugin-prettier";
-import eslintConfigPrettier from "eslint-config-prettier";
-
-export default [
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: parserTs,
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: process.cwd(),
-      },
-    },
-    plugins: {
-      "@typescript-eslint": eslintPluginTs,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...eslintPluginTs.configs.recommended.rules,
-      "prettier/prettier": "error",
-    },
-  },
-  {
-    rules: eslintConfigPrettier.rules,
-  },
-];
+// import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+// import parserTs from "@typescript-eslint/parser";
+// import prettierPlugin from "eslint-plugin-prettier";
+// import eslintConfigPrettier from "eslint-config-prettier";
+//
+// export default [
+//   {
+//     files: ["**/*.ts"],
+//     languageOptions: {
+//       parser: parserTs,
+//       parserOptions: {
+//         project: "./tsconfig.json",
+//         tsconfigRootDir: process.cwd(),
+//       },
+//     },
+//     plugins: {
+//       "@typescript-eslint": eslintPluginTs,
+//       prettier: prettierPlugin,
+//     },
+//     rules: {
+//       ...eslintPluginTs.configs.recommended.rules,
+//       "prettier/prettier": "error",
+//     },
+//   },
+//   {
+//     rules: eslintConfigPrettier.rules,
+//   },
+// ];
